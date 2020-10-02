8f2410f252d974b09d0335d8b93f4b5b0fb397eb
heth0218 <hethgala@gmail.com>
2020-10-02 20:52:24 +0530

    Updation and deletion of docs completed

--- routes/newDocs.js
+++ routes/newDocs.js
@@ -66,4 +66,89 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
+//Update the documentation
+router.post('/update/:id', auth, async (req, res) => {
+    try {
+        if (req.user.role !== 'admin') {
+            return res.status(500).send({ msg: 'You are not authorized to this action' });
+        }
+
+        const document = await NewDoc.findById(req.params.id);
+
+        if (!document) {
+            res.status(500).send({ msg: 'No such doc found in the db' });
+        }
+
+        const doc = document.doc;
+
+        const { data } = req.body;
+        const countWords = (str) => {
+            str = str.replace(/(^\s*)|(\s*$)/gi, "");
+            str = str.replace(/[ ]{2,}/gi, " ");
+            str = str.replace(/\n /, "\n");
+            return str.split(' ').length;
+        }
+
+        let contentLength = 0;
+        doc.forEach(d => {
+            if (d._id === data._id) {
+                d.text = data.text
+            }
+            const length = countWords(d.text);
+            contentLength = contentLength + length;
+
+        });
+
+        await NewDoc.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    doc,
+                    contentLength
+                },
+            },
+            {
+                new: true
+            }
+        )
+
+        const finalDoc = await NewDoc.findById(req.params.id);
+
+        if (!finalDoc) {
+            return res.status(500).send({ msg: 'Error occured!' });
+        }
+
+        return res.status(200).send(finalDoc);
+
+    } catch (error) {
+        return res.status(500).send({ msg: error.message })
+    }
+})
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let doc = await NewDoc.findById(req.params.id);
+
+        if (!doc) {
+            return res.status(404).send({ msg: 'No such doc available in the db' })
+        }
+        console.log(doc.author, req.user._id)
+
+        if (doc.author.toString() !== req.user._id.toString()) {
+            return res.status(500).send({ msg: 'Only the admin of this doc can delete it you are not authorized!' })
+
+        }
+        await NewDoc.findByIdAndDelete(req.params.id);
+
+        doc = await NewDoc.findById(req.params.id);
+        if (!doc) {
+            return res.status(200).send({ msg: 'Doc successfully deleted!' })
+        }
+
+    } catch (error) {
+        return res.status(500).send({ msg: error.message })
+
+    }
+})
+
 module.exports = router;
